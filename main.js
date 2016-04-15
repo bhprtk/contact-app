e48311fea4a2bd8571a4879bd9c62f542940f3a0
Pratik <lazymonk@Pratiks-MacBook-Air.local>
2016-04-15 00:11:10 -0700

    mvp

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(init);
+
+  function init(){
+    $('#save-changes').click(appendContacts);
+    initPopover();
+  }
+
+  function initPopover() {
+    $('.phone').popover();
+  }
+
+
+  function appendContacts(){
+    var nameVal = $('#name').val();
+    var emailVal = $('#input-email').val();
+    var phoneVal = $('#tel').val();
+    var imageurl = $('#image-url').val();
+    var $copyTemplate = $(".template").clone();
+    $('#myForm').trigger('reset');
+    $copyTemplate.removeClass('template');
+    $copyTemplate.find('.img-circle').attr("src", imageurl);
+    $copyTemplate.find('h3').text(nameVal);
+    $copyTemplate.find('.glyphicon-phone').attr("data-content", phoneVal);
+    $copyTemplate.find('.glyphicon-comment').attr("data-content", emailVal);
+
+    $('.list-group').append($copyTemplate);
+    initPopover();
+
+  }
